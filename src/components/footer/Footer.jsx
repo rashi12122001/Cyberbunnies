7fd55b0203ef18aff3b511b86cdfff97089bf4e5
rashi arora <74211043+rashi12122001@users.noreply.github.com>
2024-01-21 20:27:51 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="footer">
       <img src="" alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>Zainkeepscode@gmail.com</span>
+        <span>Rashiarora12122001@gmail.com</span>
         <div className="f-icons">
           {/* <Insta color="white" size={"3rem"} />
           <Facebook color="white" size={"3rem"} />
